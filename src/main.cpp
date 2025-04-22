216a12242355450bc6f70326681b20140a267e0b
Guzin Gunduz <guzgunduz@gmail.com>
2025-04-22 14:35:49 +0200

    Use RangeCalculator in main and update CMakeList.txt for build

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream> //Required for printing to the console
-#include "../include/CanMessage.hpp" // Include the CanMessage class we created
+#include "CanMessage.hpp"
+#include "RangeCalculator.hpp"
 
+
+//#include "../include/CanMessage.hpp" // Include the CanMessage class we created
+//Note: I dont need to use ../ anymore in the path, since I already told CMakeLists.txt to include
+// include/ directory with this line : include_directories(include) So I deleted. 
 int main() {
     //Print a message to show that the program has started
     std::cout << "EV Range Estimator Simulator Starting..." << std::endl;
@@ -11,9 +16,23 @@ int main() {
     // The vektor contains the actual data bytes
     CanMessage message(0x120, 3, {34, 89, 200 });
 
-    //Print the CAN message using the toText() method 
-    std::cout << "Fake CAN Message: " << message.toText() << std::endl;
+    //Step2:Create a RangeCalculator __cpp_lib_has_unique_object_representations
+    RangeCalculator calculator;
+
+    //Step3: Set battery level to fake value (e.g. 75%)
+    calculator.setBatteryPercentage(75);
+
+    //Step4: Calculate estimated range
+    float estimatedRange = calculator.estimateRange();
+
     
+    //Print the CAN message using the toText() method 
+    // std::cout << "Fake CAN Message: " << message.toText() << std::endl;
+   
+    //Step 5: Print Result
+    std::cout <<"Estimated Driving Range: " <<estimatedRange << "km" << std::endl;
+
 
     return 0;
 }
+ 
